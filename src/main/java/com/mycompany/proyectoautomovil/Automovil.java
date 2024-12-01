38d17973ae200aedf312e1daa6e14ffadfc0bd23
iRibaldo <ribaldo.nantipbarre3a@gmail.com>
2024-12-01 09:20:03 -0500

    Subida del inicio

---
+++ src/main/java/com/mycompany/proyectoautomovil/Automovil.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoautomovil;
+
+/**
+ *
+ * @author DELL
+ */
+public class Automovil {
+
+    public static void main(String[] args) {
+
+    }
+}
